a1ae6a65b865114c781ba23dd670b94b706c5e35
Leonardo Oliveira <lfmaker@outlook.com>
2020-08-19 00:32:16 -0300

    feat: add install_requires

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,6 +1,7 @@
-import pandas as pd
-from imblearn.over_sampling import SMOTE
 from sklearn.base import BaseEstimator, TransformerMixin
+import imblearn
+from imblearn.over_sampling import SMOTE
+import pandas as pd
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -15,33 +16,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
-
-class ConverterColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        data["INGLES"] = pd.to_numeric(data["INGLES"])
-        data["FALTAS"] = pd.to_numeric(df_data_3["FALTAS"])
-        data["REPROVACOES_DE"] = pd.to_numeric(data["REPROVACOES_DE"])
-        data["REPROVACOES_EM"] = pd.to_numeric(data["REPROVACOES_EM"])
-        data["REPROVACOES_MF"] = pd.to_numeric(data["REPROVACOES_MF"])
-        data["REPROVACOES_GO"] = pd.to_numeric(data["REPROVACOES_GO"])
-        data["TAREFAS_ONLINE"] = pd.to_numeric(data["TAREFAS_ONLINE"])
-        data["H_AULA_PRES"] = pd.to_numeric(data["H_AULA_PRES"])
-        data["NOTA_DE"] = pd.to_numeric(data["NOTA_DE"], downcast="float")
-        data["NOTA_EM"] = pd.to_numeric(data["NOTA_EM"], downcast="float")
-        data["NOTA_MF"] = pd.to_numeric(data["NOTA_MF"], downcast="float")
-        data["NOTA_GO"] = pd.to_numeric(data["NOTA_GO"], downcast="float")
-        return data;
-
-
+    
 class SetIndex(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
@@ -54,3 +29,13 @@ class SetIndex(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.set_index(self.columns, inplace=True)
+    
+   
+class SmoteResample(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        X_resampled, y_resampled = SMOTE().fit_resample(X, y)
+        X_resampled = pd.DataFrame(X_resampled, columns=X.columns)
+        return X_resampled, y_resampled
